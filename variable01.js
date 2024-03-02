3c3cd928b40a6beb593199e098b16e95ed138169
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2024-03-02 19:56:18 +0600

    learing js

--- variable01.js
+++ variable01.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+/* console.log("Hello World!") */
+const usersID = 12457
+let usersEmail = "omar02@gmail.com"
+console.log(usersID)
+console.log(usersEmail)
+// usersID = 89076 Not Allowed
+console.log(usersID)
+
+
+usersEmail="bin782gmail.com"
+console.log(usersEmail)
+console.table([usersID,usersEmail])
