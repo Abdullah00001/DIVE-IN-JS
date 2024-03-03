5ca7ed640b128d2fb3afd680014add587b510c42
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2024-03-04 04:45:05 +0600

    understanding with string

--- string.js
+++ string.js
@@ -2,7 +2,18 @@ const usersName = "Abdullah Bin Omar Chowdhury"
 const usersCollege = "National Polytechnique Instititue"
 let usersDetails = `hello my name ${usersName} .i am studied in ${usersCollege}`
 console.log(usersDetails)
-let usersLoacation="dhaka"
+let usersLoacation = "dhaka"
 console.log(usersLoacation[0])
-let usersAddress=new String('Rangpur')
+let usersAddress = new String('Rangpur')
 console.log(usersAddress[4])
+let usersCity = "Dhaka "
+console.log(usersCity.replace("Dha", "Ran"))
+console.log(usersCity.trim())
+console.log(usersCity.includes("m"))
+let someInfo = "Sunset - paints - sky - with - hues - of - crimson - and - gold-Whispers - of - wind - dance - through - leaves - of - ancient - trees"
+console.log(someInfo.split("-"))
+console.log(someInfo[9])
+console.log(someInfo.toLowerCase())
+console.log(someInfo.toUpperCase())
+console.log(someInfo.substring(4, 16))
+console.log(someInfo.endsWith("dhaka", 15))
