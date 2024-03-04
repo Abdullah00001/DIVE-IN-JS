e3d018966743ff64cf799fa6a4f86be70b6a3dd9
ABDULLAH <abdullahbinomarchowdhury02@gmail.com>
2024-03-04 15:29:23 +0600

    now have a efficient basement of array

--- array.js
+++ array.js
@@ -1,15 +1,31 @@
 let arr = ["s", "o", "m", "u", "d", "r", "o"]
-// for (let i = 0; i < arr.length; i++) console.log(arr[i])
-/* for (let i = arr.length-1;i>=0; i--) console.log(arr[i])
+for (let i = 0; i < arr.length; i++) console.log(arr[i])
+for (let i = arr.length-1;i>=0; i--) console.log(arr[i])
 arr[0]="a"
 for (let i = 0; i < arr.length; i++) console.log(i," : ",arr[i])
 arr.push("b","i","n")
 for (let i = 0; i < arr.length; i++) console.log(i," : ",arr[i])
 if(arr.includes("A")) console.log("ITS TIME TO GO")
 else console.log("ITS TIME TO END")
- */
+
 let arr1 = [10, 30, 20, 60, 90, 40, 100, 50]
 let arr2 = []
 for (let i = 2; i <= 6; i++) arr2[i-2] = arr1[i]
 for (let i = 0; i < arr2.length; i++) console.log(i, ":", arr2[i])
-// console.log(arr2[0])
+console.log(arr2[0])
+for(let a of arr1){
+    console.log(a)
+}
+let foodItem = ["burger", "pizza", "sandwich", "momo"]
+let normalFood = ["rice", "beaf", "mutton", "biriyani"]
+for (let itemsOfArr of normalFood) {
+    foodItem.push(itemsOfArr)
+}
+for (let i = 0; i < foodItem.length; i++) {
+    console.log(foodItem[i])
+}
+console.log(foodItem)
+let allItems=foodItem.concat(normalFood)
+console.log(allItems)
+let allRecipie = [...foodItem, ...normalFood]
+console.log(allRecipie)
